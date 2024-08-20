ff059a0fa94850f0362582bb52b2cae1cf49efba
menescalnickolas <menescalnickolas@gmail.com>
2024-08-20 11:15:23 +0200

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,26 @@ import EventList from './components/EventList';
 import './App.css';
 import { mockData } from './mock-data';
 import NumberOfEvents from './components/NumberOfEvents';
+import { useEffect, useState } from 'react';
+import { getEvents } from './api';
 
 const App = () => {
+  const [events, setEvents] = useState([]);
+  const [currentNOE, setCurrentNOE] = useState([32]);
+
+  const fetchData = async () => {
+    const allEvents = await getEvents();
+    setEvents(allEvents.slice(0, currentNOE));
+}
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <CitySearch />
-      <EventList events={mockData}/>
+      <EventList events={events}/>
       <NumberOfEvents />
     </div>
   );
