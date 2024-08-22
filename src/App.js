c6bb62006cdf2295dab1ed5051a5ff241b5876f5
menescalnickolas <menescalnickolas@gmail.com>
2024-08-22 17:40:47 +0200

    Tried solving test failure

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
-import './App.css';
 import { mockData } from './mock-data';
 import NumberOfEvents from './components/NumberOfEvents';
 import { useEffect, useState } from 'react';
 import { getEvents } from './api';
+import './App.css';
+
 
 const App = () => {
   const [events, setEvents] = useState([]);
-  const [currentNOE, setCurrentNOE] = useState([32]);
+  const [currentNOE, setCurrentNOE] = useState(32);
 
   const fetchData = async () => {
-    const allEvents = await getEvents();
+    const allEvents = await getEvents() || [];
     setEvents(allEvents.slice(0, currentNOE));
 }
 
@@ -23,7 +24,7 @@ const App = () => {
   return (
     <div className="App">
       <CitySearch />
-      <EventList events={events}/>
+      <EventList events={events} />
       <NumberOfEvents />
     </div>
   );
