6ae43285c2412689dec09484a9ce912d503be2e6
menescalnickolas <menescalnickolas@gmail.com>
2024-08-24 17:22:39 +0200

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -4,26 +4,33 @@ import EventList from './components/EventList';
 import { mockData } from './mock-data';
 import NumberOfEvents from './components/NumberOfEvents';
 import { useEffect, useState } from 'react';
-import { getEvents } from './api';
+import { getEvents,extractLocations } from './api';
+
 import './App.css';
 
 
 const App = () => {
   const [events, setEvents] = useState([]);
   const [currentNOE, setCurrentNOE] = useState(32);
+  const [allLocations, setAllLocations] = useState([]);
+  const [currentCity, setCurrentCity] = useState("See all cities");
 
   const fetchData = async () => {
-    const allEvents = await getEvents() || [];
-    setEvents(allEvents.slice(0, currentNOE));
-}
+    const allEvents = await getEvents();
+    const filteredEvents = currentCity === "See all cities" ?
+      allEvents :
+      allEvents.filter(event => event.location === currentCity)
+    setEvents(filteredEvents.slice(0, currentNOE));
+    setAllLocations(extractLocations(allEvents));
+  }
 
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [currentCity]);
 
   return (
     <div className="App">
-      <CitySearch />
+      <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity}/>
       <EventList events={events} />
       <NumberOfEvents />
     </div>
