f85b2be159bbc879f279a056fd9a53d1c5424f54
menescalnickolas <menescalnickolas@gmail.com>
2024-09-14 18:32:43 +0200

    Added describe('filter events by city')

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -1,14 +1,16 @@
 import puppeteer from "puppeteer-core";
 
+jest.setTimeout(50000); 
+
 describe('show/hide an event details', () => {
   let browser;
   let page;
     beforeAll(async () => {
       browser = await puppeteer.launch({
         channel: 'chrome',
-        headless: false,
+        headless: true,
         slowMo: 250,
-        timeout: 0
+        timeout: 0 
       });
       page = await browser.newPage();
       await page.goto('http://localhost:3000/');
@@ -35,5 +37,43 @@ describe('show/hide an event details', () => {
     await page.click('.event .details-btn');
     const eventDetails = await page.$('.event .details');
     expect(eventDetails).toBeNull();
+  });
+});
+
+
+describe('filter events by city', () => {
+  let browser;
+  let page;
+    beforeAll(async () => {
+      browser = await puppeteer.launch({
+        channel: 'chrome',
+        headless: true,
+        slowMo: 250,
+        timeout: 0 
+      });
+      page = await browser.newPage();
+      await page.goto('http://localhost:3000/');
+      await page.waitForSelector('.event');
+    });
+
+    afterAll(() => {
+      browser.close();
+    });
+
+  test('When user hasn’t searched for a city, show upcoming events from all cities.', async () => {
+    const events = await page.$('.event'); 
+    expect(events).toBeDefined();
+  });
+
+  test('User should see a list of suggestions when they search for a city.', async () => {
+    await page.type('.city', 'Berlin Germany');
+    const suggestions = await page.$('.suggestions');
+    expect(suggestions).toBeDefined();
+  });
+
+  test('User can select a city from the suggested list.', async () => {
+    await page.click('.suggestions li');
+    const city = await page.$('.city');
+    expect(city).toBeDefined();
   });
 });
