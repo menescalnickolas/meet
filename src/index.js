0cb03fc96e2a64d9905143ead751b0061b4162ae
menescalnickolas <menescalnickolas@gmail.com>
2024-10-13 15:08:40 +0200

    Added navigator line

--- src/index.js
+++ src/index.js
@@ -20,6 +20,8 @@ root.render(
 // Learn more about service workers: https://cra.link/PWA
 serviceWorkerRegistration.register();
 
+if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator)
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
