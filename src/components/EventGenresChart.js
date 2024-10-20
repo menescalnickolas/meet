e85ef525a8e65509b7017eeb26ef838f3d9bc715
menescalnickolas <menescalnickolas@gmail.com>
2024-10-20 13:31:24 +0200

    Created EventGenresChart component

---
+++ src/components/EventGenresChart.js
@@ -0,0 +1,61 @@
+import { useState, useEffect } from 'react';
+import React, { PureComponent } from 'react';
+import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from 'recharts';
+
+const EventGenresChart = ({ events }) => {
+
+  const [data, setData] = useState([]);
+  const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'Angular']; 
+
+  useEffect(() => {
+    setData(getData());
+  }, [`${events}`]);
+
+  const getData = () => {
+    const data = genres.map((genre) => {
+      const filteredEvents = events.filter((event) => event.summary.includes(genre));
+      return {
+        name: genre,
+        value: filteredEvents.length
+      };
+    });
+    return data;
+  };
+
+  const renderCustomizedLabel = ({ cx, cy, midAngle, outerRadius, innerRadius, percent, index }) => {
+    const RADIAN = Math.PI / 180;
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.7;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+    return percent ? (
+      <text
+        x={x}
+        y={y}
+        fill="#fff"
+        textAnchor={x > cx ? 'start' : 'end'}
+        dominantBaseline="central"
+      >
+        {`${(percent * 100).toFixed(0)}%`}
+      </text>
+    ) : null;
+  };
+
+  return (
+    <ResponsiveContainer width="99%" height={400}>
+      <PieChart>
+        <Pie
+          data={data}
+          dataKey="value"
+          fill="#8884d8"
+          labelLine={false}
+          label={renderCustomizedLabel}
+          outerRadius={130}           
+        />
+      </PieChart>
+    </ResponsiveContainer>
+  );
+
+
+}
+
+export default EventGenresChart;
